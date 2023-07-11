66edd503af92dc7ba409a09a8099d26ecf1cb59c
Andre M. de Souza <andresouza44@gmail.com>
2023-07-11 15:43:14 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -16,19 +16,23 @@ public class Game {
     @Column(name="game_year")
     private Integer year;
     private String genre;
-    private String plataforms;
+    private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column (columnDefinition = "TEXT")
     private String ShortDescription;
+
+    @Column (columnDefinition = "TEXT")
     private String longDescription;
 
-    public Game(Long id, String title, Integer year, String genre, String plataforms,
+    public Game(Long id, String title, Integer year, String genre, String platforms,
                 Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataforms = plataforms;
+        this.platforms = platforms;
         this.score = score;
         this.imgUrl = imgUrl;
         ShortDescription = shortDescription;
@@ -67,12 +71,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlataforms() {
-        return plataforms;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlataforms(String plataforms) {
-        this.plataforms = plataforms;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public Double getScore() {
