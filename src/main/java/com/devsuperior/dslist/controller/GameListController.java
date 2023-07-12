7e0550dcff94688f1c77edb75b6feed03b4f4133
Andre M. de Souza <andresouza44@gmail.com>
2023-07-12 12:14:29 -0300

    Get lists

---
+++ src/main/java/com/devsuperior/dslist/controller/GameListController.java
@@ -0,0 +1,29 @@
+package com.devsuperior.dslist.controller;
+
+import com.devsuperior.dslist.dto.GameDTO;
+import com.devsuperior.dslist.dto.GameListDTO;
+import com.devsuperior.dslist.dto.GameMinDto;
+import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.services.GameListService;
+import com.devsuperior.dslist.services.GameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value ="/lists")
+public class GameListController {
+
+    @Autowired
+    private GameListService gameListService;
+
+
+    @GetMapping
+    public List <GameListDTO> findAll(){
+        return gameListService.findAll();
+    }
+}
